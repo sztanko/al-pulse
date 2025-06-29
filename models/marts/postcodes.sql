6c1429658cb5ca235d9c33683bde5e750cd37d6f
Dimi <sztanko@gmail.com>
2025-06-29 08:59:17 +0100

    Increase mem limits

--- models/marts/postcodes.sql
+++ models/marts/postcodes.sql
@@ -4,9 +4,9 @@
         pre_hook=[
             "DROP INDEX IF EXISTS idx_postcode_areas_geom",
             "SET preserve_insertion_order = FALSE",
-            "SET memory_limit = '3GB'",
+            "SET memory_limit = '6GB'",
             "PRAGMA temp_directory = '/tmp'",
-            "PRAGMA max_temp_directory_size = '10GB"
+            "PRAGMA max_temp_directory_size = '10GB'"
         ],
         post_hook='CREATE INDEX IF NOT EXISTS idx_postcode_areas_geom ON {{ this }} USING RTREE(geom)'
         
