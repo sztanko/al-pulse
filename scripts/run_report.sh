05470a60b8b15754906cd55b81aaf52f3cf78ab5
Dimi <sztanko@gmail.com>
2025-06-29 18:42:34 +0100

    Add sources generation

--- scripts/run_report.sh
+++ scripts/run_report.sh
@@ -1,6 +1,12 @@
 #!/bin/bash -e
 
+echo "Exporting data to GeoJSON..."
 ./scripts/export_to_geojson.sh localities_with_data_for_geojson
 cd reports
+echo "Installing dependencies..."
 npm install
+echo "Generating sources..."
+npm run sources
+echo "Generating report..."
 npm run build
+echo "Report generation completed successfully."
